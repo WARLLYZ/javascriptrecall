08322b5785f769567cfaf052a7407ab2e09a14c2
WARLLYZ <117789209+WARLLYZ@users.noreply.github.com>
2022-12-09 18:18:09 -0500

    first

---
+++ totalrecall.js
@@ -0,0 +1,58 @@
+
+
+// 1. How do we assign a value to a variable? 
+//     We use an "=" then assign the value.
+// 2. How do we change the value of a variable? 
+//     i would use the assignment operator "=" and then the new value of the variable
+// 3. How do we assign an existing variable to a new variable? 
+//     i would use the assignment operator "=" after the name of the new variable then the name of the existing variable
+// 4. Remind me, what are declare, assign, and define? 
+//    In JavaScript, declaring a variable means creating a variable and giving it a name, while defining a variable means assigning a value to it 
+// 5. What is pseudocoding and why should you do it? 
+//    Pseudocoding is a valuable tool for anyone who is developing algorithms or programs, and can help you to write better, more efficient code. 
+// 6. What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it 
+//     I would say around 30% of your time thinking about how to solve a problem before you start writing code.
+
+
+//b strings
+
+let firstVariable = "Hello World";
+firstVariable = 7;
+let secondVariable = firstVariable;
+secondVariable = "Hello again";
+
+//the value for my first car was changed to 7 btw
+console.log(firstVariable);
+
+let yourName = "Daniel-nathen Yamamoto";
+let greeting = "Hello, my name is " + yourName;
+console.log(greeting);
+
+//c boolean
+
+const a = 4;
+const b = 53;
+const c = 57;
+const d = 16;
+const e = 'Kevin';
+
+console.log(a < b); 
+console.log(c > d); 
+console.log('Name' === 'Name');
+console.log(a + b === c); 
+console.log(a * a === d);
+console.log(e === 'Kevin');
+//all logs should be true above ^^
+
+//d the farm
+let animal = "cow";
+if (animal === "cow") {
+    console.log("mooooo");
+}
+if (animal === "cow") {
+    console.log("mooooo");
+} else {
+    console.log("Hey! You're not a cow.");
+}
+
+// committing 
